144e1d4277d5d9fb11deedd83526139cca54b360
ElCollo <isanbaev_ildar@mail.ru>
2022-07-23 19:27:06 +0700

    .

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css'
 
 const Header = () => {
-    return <header className="header">
+    return <header className={s.header}>
                 <img src="https://img1.freepng.ru/20180331/tfw/kisspng-luck-sticker-friday-the-13th-decal-superstition-lucky-5abf265e9561f7.7271344115224766386119.jpg"/>
             </header>
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className="content">
+    return <div className={s.content}>
         <div>
             <img src="https://images.drive.ru/i/3/6172c8bf1143d64b5fb9bf5f"/>
         </div>
         <div> ava + description</div>
         <div> My posts
             <div>New posts</div>
-            <div>
-                <div>
+            <div className={s.posts}>
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     post 2
                 </div>
             </div>
