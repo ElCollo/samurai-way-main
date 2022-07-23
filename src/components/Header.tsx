aaa0fb7e21e255d9a2a1b356b5e4bcf0a46b0be0
ElCollo <isanbaev_ildar@mail.ru>
2022-07-23 17:47:18 +0700

    Создаем сайт с помощью React JS

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+                <img src="https://img1.freepng.ru/20180331/tfw/kisspng-luck-sticker-friday-the-13th-decal-superstition-lucky-5abf265e9561f7.7271344115224766386119.jpg"/>
+            </header>
+}
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src="https://images.drive.ru/i/3/6172c8bf1143d64b5fb9bf5f"/>
+        </div>
+        <div> ava + description</div>
+        <div> My posts
+            <div>New posts</div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+export default Profile
